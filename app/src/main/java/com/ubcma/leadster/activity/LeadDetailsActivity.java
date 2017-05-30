c0b680eb61c8eb16066879eaf322f0ab570cc3be
Yvonne <princess0115@gmail.com>
2017-05-30 12:41:45 -0400

    added more icons  added a spinner

--- app/src/main/java/com/ubcma/leadster/activity/LeadDetailsActivity.java
+++ app/src/main/java/com/ubcma/leadster/activity/LeadDetailsActivity.java
@@ -6,8 +6,12 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.ubcma.leadster.R;
+
+
 
 public class LeadDetailsActivity extends AppCompatActivity {
 
@@ -26,5 +30,13 @@ public class LeadDetailsActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Spinner spinner = (Spinner) findViewById(R.id.cld_attempts_detail);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.numbers_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
