a895cab1fe11f2666541ecd994ac313ca539bf96
Yvonne <princess0115@gmail.com>
2017-06-21 18:04:19 -0400

    added up navigation to goals activity. Modified goals list to display only a list of goals added

--- app/src/main/java/com/ubcma/leadster/activity/GoalsActivity.java
+++ app/src/main/java/com/ubcma/leadster/activity/GoalsActivity.java
@@ -3,8 +3,10 @@ package com.ubcma.leadster.activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -24,6 +26,10 @@ public class GoalsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_goals);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_goals);
         setSupportActionBar(toolbar);
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_goals);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -42,12 +48,24 @@ public class GoalsActivity extends AppCompatActivity {
     public List<String> getLabels(){
 
         List<String> labels = new ArrayList<>();
-        labels.add("Calls");
-        labels.add("Interviews");
-        labels.add("Appointments");
-        labels.add("Recuits");
+        labels.add("Calls per week");
+        labels.add("Interviews per week");
+        labels.add("Appointments per week");
+        labels.add("Recuits per week");
 
         return labels;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            //no menu resource file needed for up navigation
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
--- app/src/main/java/com/ubcma/leadster/adapter/GoalsAdapter.java
+++ app/src/main/java/com/ubcma/leadster/adapter/GoalsAdapter.java
@@ -21,37 +21,39 @@ import java.util.List;
 
 public class GoalsAdapter extends ArrayAdapter<String> {
 
-    ArrayAdapter<CharSequence> mSpinnerAdapter;
     List<String> mLabels;
+    Context mContext;
 
     public GoalsAdapter(@NonNull Context context, @NonNull List<String> labels) {
         super(context, 0, labels);
         mLabels = labels;
-
-        mSpinnerAdapter = ArrayAdapter.createFromResource(getContext(),
-                R.array.goal_interval_array, android.R.layout.simple_spinner_item);
-
-        // Specify the layout to use when the list of choices appears
-        mSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mContext = context;
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
+        TextView goalsLabel, goalCount;
         View goalsItemView = convertView;
-        if(goalsItemView != null){
-            goalsItemView = LayoutInflater.from(getContext()).inflate(
-                    R.layout.goal_row, parent, false);
+        if(goalsItemView == null){
+            LayoutInflater inflater = (LayoutInflater) mContext
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            goalsItemView = inflater.inflate(R.layout.goal_row, parent, false);
         }
 
-        TextView goalsLabel = (TextView) goalsItemView.findViewById(R.id.calls_goal_label);
+        goalsLabel = (TextView) goalsItemView.findViewById(R.id.calls_goal_label);
         goalsLabel.setText(mLabels.get(position));
 
-        Spinner goalsSpinner = (Spinner) goalsItemView.findViewById(R.id.calls_goal_spinner);
+        goalCount = (TextView) goalsItemView.findViewById(R.id.goal_count);
+        goalCount.setText("4");
 
-        // Apply the mSpinnerAdapter to the spinner
-        goalsSpinner.setAdapter(mSpinnerAdapter);
+
         return goalsItemView;
+    }
+
+    @Override
+    public int getCount() {
+        return mLabels.size();
     }
 }
